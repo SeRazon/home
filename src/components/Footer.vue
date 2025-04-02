f35660450c91ce84515ff3c57aac04f06dc94b1d
星鸿 <weizixuan110@foxmail.com>
2025-04-02 20:48:04 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <Transition name="fade" mode="out-in">
       <div v-if="!store.playerState || !store.playerLrcShow" class="power">
         <span>
-          <span :class="startYear < fullYear ? 'c-hidden' : 'hidden'">Copyright&nbsp;</span>
+          <span :class="startYear < fullYear ? 'c-hidden' : 'hidden'">星鸿_SeRazon 2024-2025 Copyright</span>
           &copy;
           <span v-if="startYear < fullYear"
             class="site-start">
